58c38efac254c7044bcabf8fcccc3176608c03c2
Chris Carr - Home Laptop <ccarr895@gmail.com>
2022-06-06 12:24:29 +0100

    began work on audio

---
+++ src/Encoder/Encoder.py
@@ -0,0 +1,8 @@
+import pvleopard
+
+def get_audio():
+    return
+
+leopard = pvleopard.create(access_key='OX9bSVHcnyOpSrYKfmrJOh43DAg7d7kQ6BP0PWatLJMXgdOQrmoNOA==')
+
+print(leopard.process_file(get_audio()))
